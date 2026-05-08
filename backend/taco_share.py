a46349db4589f97f200574dd0c5a4808a51f1ceb
Langmiao <langkoumiao@outlook.com>
2026-05-08 16:38:20 +1000

    Fix database initialization on Cloud Run

--- backend/taco_share.py
+++ backend/taco_share.py
@@ -3,7 +3,7 @@ import sqlite3
 import hashlib
 import random
 import time
-import os
+
 
 app = Flask(__name__)
 
@@ -128,8 +128,9 @@ def get_shared(pin):
 
 
 
+import os
 
+init_db()
 
 if __name__ == "__main__":
-    init_db()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)),debug=False)
